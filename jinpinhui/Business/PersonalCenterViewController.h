4db67b4ec9fc4276853ba80bdb5eeb70f09610e0
yhc928 <yuhaichao928@163.com>
2015-07-14 23:13:05 +0800

    yhc

---
+++ jinpinhui/Business/PersonalCenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonalCenterViewController.h
+//  jinpinhui
+//
+//  Created by 于海超 on 15/7/14.
+//  Copyright (c) 2015年 chenzhen. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface PersonalCenterViewController : BaseViewController
+
+@end
